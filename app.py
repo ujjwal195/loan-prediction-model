60b92de91292cb654fa172d23d9fdbc01a27289e
ujjwal195 <sanchetiujjwal@gmail.com>
2025-05-08 17:17:56 +0530

    Updated app.py to use dynamic port

--- app.py
+++ app.py
@@ -74,4 +74,7 @@ def predict_api():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use the environment's PORT variable or fallback to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
